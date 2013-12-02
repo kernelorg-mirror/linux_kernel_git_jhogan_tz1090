5c078da98438c40e9844f8cc72f076c237e7669f
James Hogan <james.hogan@imgtec.com>
2013-12-02 23:45:35 +0000

    dt: binding: Add binding for TZ1090 PDC clocks
    The TZ1090 PDC (PowerDown Controller) has a couple of clock components
    in order to generate its main 32.768KHz clock which the peripherals in
    the low power domain run off, especially while the SoC is otherwise
    powered down.
    
    Create a clock binding for the provider of these clocks, and a header
    file in <dt-bindings/clock/> for enumerating the provided clocks.
    
    It essentially has just two clock inputs (xtal1 and xtal3), and two
    clock outputs, of which only "32khz" is likely to be used.
    
    Signed-off-by: James Hogan <james.hogan@imgtec.com>
    Cc: Mike Turquette <mturquette@linaro.org>
    Cc: Rob Herring <robh+dt@kernel.org>
    Cc: Pawel Moll <pawel.moll@arm.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Ian Campbell <ijc+devicetree@hellion.org.uk>
    Cc: Kumar Gala <galak@codeaurora.org>
    Cc: linux-metag@vger.kernel.org
    Cc: devicetree@vger.kernel.org
    ---
    Changes in v3:
    - Rename to pdc-cr and add syscon to compatible string.
    
    Changes since v1 (patch 11):
    - Change the binding to conceptually represent all clocks provided by
      the PDC in TZ1090 rather than the same generic arrangement of clocks
      as the PDC in TZ1090, since the driver will use the common TZ1090
      specific clock provider helpers to provide specific clocks.
    - Change compatible string from "img,tz1090-pdc-clock" to
      "img,tz1090-pdc-clocks" to match the intended purpose.
    - Add header in <dt-bindings/clock/> to enumerate the provided clocks.
    - Add clock-names as required property as used by common TZ1090 clock
      provider code.
    - Mention standard clock-indices and clock-output-names optional
      properties for when the clocks may be used by other clock providers
      and should match the names used in the driver.

---
+++ include/dt-bindings/clock/tz1090-pdc.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2013-2014 Imagination Technologies Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _DT_BINDINGS_CLK_TZ1090_PDC_H
+#define _DT_BINDINGS_CLK_TZ1090_PDC_H
+
+/* Powerdown Controller clocks */
+#define CLK_PDC_XTAL1_DIV	0
+#define CLK_PDC_32KHZ		1
+
+#define CLK_PDC_MAX		2
+
+#endif

6f5d78ec5210a4412f57a0bc2c649320a1b228a2
James Hogan <james.hogan@imgtec.com>
2014-11-28 00:38:23 +0000

    dt: binding: Add binding for TZ1090 PERIP clocks
    The TZ1090 peripheral (PERIP) register region controls a bank of system
    clock gates for individual peripherals.
    
    Create a clock binding for the provider of these clocks, and a header
    file in <dt-bindings/clock/> for enumerating the provided clocks.
    
    There is a single input clock, the main system clock, and multiple
    peripheral specific system clocks provided which should be referenced by
    the relevant peripheral device nodes.
    
    Signed-off-by: James Hogan <james.hogan@imgtec.com>
    Cc: Mike Turquette <mturquette@linaro.org>
    Cc: Rob Herring <robh+dt@kernel.org>
    Cc: Pawel Moll <pawel.moll@arm.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Ian Campbell <ijc+devicetree@hellion.org.uk>
    Cc: Kumar Gala <galak@codeaurora.org>
    Cc: linux-metag@vger.kernel.org
    Cc: devicetree@vger.kernel.org
    ---
    Changes in v3:
    - Rename to perip-cr and add syscon to compatible string.
    
    Changes since v1 (patch 15):
    - New patch.
    - Convert explicit DT representation of clock infrastructure using
      generic bindings to several TZ1090 specific bindings representing
      groups of TZ1090 clocks.

---
+++ include/dt-bindings/clock/tz1090-perip.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2013-2014 Imagination Technologies Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _DT_BINDINGS_CLK_TZ1090_PERIP_H
+#define _DT_BINDINGS_CLK_TZ1090_PERIP_H
+
+/* CR_PERIP_CLKEN - Peripheral system clocks */
+#define CLK_PERIP_SCB0		 0
+#define CLK_PERIP_SCB1		 1
+#define CLK_PERIP_SCB2		 2
+#define CLK_PERIP_SDIO		 3
+#define CLK_PERIP_UART0		 4
+#define CLK_PERIP_UART1		 5
+#define CLK_PERIP_SPIM		 6
+#define CLK_PERIP_SPIS		 7
+#define CLK_PERIP_SPIM1		 8
+#define CLK_PERIP_I2SOUT	 9
+#define CLK_PERIP_I2SIN		10
+#define CLK_PERIP_LCD		11
+#define CLK_PERIP_SDHOST	12
+#define CLK_PERIP_USB		13
+
+#define CLK_PERIP_MAX		14
+
+#endif

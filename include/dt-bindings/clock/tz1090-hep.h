c814889d49ebaebb7387ca90dfacfa5656da7e4e
James Hogan <james.hogan@imgtec.com>
2014-11-28 00:37:44 +0000

    dt: binding: Add binding for TZ1090 HEP clocks
    The TZ1090 High End Peripheral (HEP) register region controls several
    clocks for the HEP peripherals.
    
    Create a clock binding for the provider of these clocks, and a header
    file in <dt-bindings/clock/> for enumerating the provided clocks.
    
    There are 3 clocks provided, the 2D block clock, the PDP/PDI clocks, and
    the DDR enable clock which is fed back into the TOP clocks for dividing
    down.
    
    Signed-off-by: James Hogan <james.hogan@imgtec.com>
    Cc: Mike Turquette <mturquette@linaro.org>
    Cc: Rob Herring <robh+dt@kernel.org>
    Cc: Pawel Moll <pawel.moll@arm.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Ian Campbell <ijc+devicetree@hellion.org.uk>
    Cc: Kumar Gala <galak@codeaurora.org>
    Cc: linux-metag@vger.kernel.org
    Cc: devicetree@vger.kernel.org
    ---
    Changes in v3:
    - Rename to hep-cr and add syscon to compatible string.
    
    Changes since v1 (patch 15):
    - New patch.
    - Convert explicit DT representation of clock infrastructure using
      generic bindings to several TZ1090 specific bindings representing
      groups of TZ1090 clocks.
    
    Rename top clock binding to top-cr and syscon

---
+++ include/dt-bindings/clock/tz1090-hep.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2013-2014 Imagination Technologies Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _DT_BINDINGS_CLK_TZ1090_HEP_H
+#define _DT_BINDINGS_CLK_TZ1090_HEP_H
+
+/* CR_HEP_CLK_EN - High End Peripheral clocks */
+#define CLK_HEP_2D		0
+#define CLK_HEP_DDR_EN		1
+#define CLK_HEP_PDP		2
+
+#define CLK_HEP_MAX		3
+
+#endif
